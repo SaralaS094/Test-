c4e932e42a7becfbc3cd515b86a468633da30c95
Sarala R <sarala.r@prateektechnosoft.com>
2025-04-22 17:51:56 +0530

    Version 1 updated

--- MyfirstTest.js
+++ MyfirstTest.js
@@ -1,7 +1,14 @@
 console.log("Hello User !!!");
 function sum(a,b){
   
-   return a+b;
+  return a+b;
+  
 }
-sum(10,5);
-console.log(sum)
+var c = sum(10,5);
+console.log(c);
+
+function sub(a,b){
+  return a-b;
+}
+var d = sub(5,10);
+console.log(d);
